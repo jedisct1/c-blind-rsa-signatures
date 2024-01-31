755fad8df7b510a509317ec4fa8744042c521e6d
Frank Denis <github@pureftpd.org>
2024-01-31 22:27:18 +0100

    Update zig build file for Zig 0.12

--- build.zig
+++ build.zig
@@ -4,20 +4,76 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const lib_options = b.addOptions();
+    const with_boringssl = b.option([]const u8, "with-boringssl", "Path to BoringSSL install") orelse "";
+    lib_options.addOption([]const u8, "with-boringssl", with_boringssl);
+
     const lib = b.addStaticLibrary(.{
-        .name = "blind_rsa",
+        .name = "brsa",
         .target = target,
         .optimize = optimize,
     });
-    b.installArtifact(lib);
+
     lib.linkLibC();
-    lib.addIncludePath(.{ .path = "/opt/homebrew/opt/openssl/include" });
-    lib.addLibraryPath(.{ .path = "/opt/homebrew/opt/openssl/lib" });
-    lib.linkSystemLibrary("crypto");
-    const source_files = &.{"src/blind_rsa.c"};
-    if (@hasDecl(std.Build.Step.Compile, "AddCSourceFilesOptions")) {
-        lib.addCSourceFiles(.{ .files = source_files });
+    if (with_boringssl.len > 0) {
+        var buf_include: [std.os.PATH_MAX]u8 = undefined;
+        var buf_include_alloc = std.heap.FixedBufferAllocator.init(&buf_include);
+        const path_include = try std.fs.path.join(buf_include_alloc.allocator(), &.{ with_boringssl, "include" });
+
+        var buf_lib: [std.os.PATH_MAX]u8 = undefined;
+        var buf_lib_alloc = std.heap.FixedBufferAllocator.init(&buf_lib);
+        const path_lib = try std.fs.path.join(buf_lib_alloc.allocator(), &.{ with_boringssl, "lib" });
+
+        lib.addIncludePath(.{ .path = path_include });
+        lib.addLibraryPath(.{ .path = path_lib });
     } else {
-        lib.addCSourceFiles(source_files, &.{});
+        lib.addIncludePath(.{ .path = "/opt/homebrew/opt/openssl@3/include" });
+        lib.addLibraryPath(.{ .path = "/opt/homebrew/opt/openssl@3/lib" });
     }
+    lib.linkSystemLibrary("crypto");
+
+    const lib_source_files = &.{"src/blind_rsa.c"};
+
+    lib.addCSourceFiles(.{ .files = lib_source_files });
+    b.installArtifact(lib);
+
+    const exe_source_files = &.{"src/test_blind_rsa.c"};
+
+    const exe = b.addExecutable(.{
+        .name = "c-blind-rsa-signatures",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    if (with_boringssl.len > 0) {
+        var buf_include: [std.os.PATH_MAX]u8 = undefined;
+        var buf_include_alloc = std.heap.FixedBufferAllocator.init(&buf_include);
+        const path_include = try std.fs.path.join(buf_include_alloc.allocator(), &.{ with_boringssl, "include" });
+
+        var buf_lib: [std.os.PATH_MAX]u8 = undefined;
+        var buf_lib_alloc = std.heap.FixedBufferAllocator.init(&buf_lib);
+        const path_lib = try std.fs.path.join(buf_lib_alloc.allocator(), &.{ with_boringssl, "lib" });
+
+        exe.addIncludePath(.{ .path = path_include });
+        exe.addLibraryPath(.{ .path = path_lib });
+    } else {
+        exe.addIncludePath(.{ .path = "/opt/homebrew/opt/openssl@3/include" });
+        exe.addLibraryPath(.{ .path = "/opt/homebrew/opt/openssl@3/lib" });
+    }
+    exe.addCSourceFiles(.{ .files = exe_source_files });
+    exe.linkLibrary(lib);
+    exe.linkLibC();
+
+    const run_cmd = b.addRunArtifact(exe);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_exe_unit_tests = b.addRunArtifact(exe);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_exe_unit_tests.step);
 }
