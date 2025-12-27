bbce361547caed643ff5d735b80ed95cb36bdb55
Frank Denis <github@pureftpd.org>
2025-12-27 17:20:37 +0100

    Update for ZIg 0.16

--- build.zig
+++ build.zig
@@ -11,7 +11,27 @@ pub fn build(b: *std.Build) !void {
     const lib_module = b.createModule(.{
         .target = target,
         .optimize = optimize,
+        .link_libc = true,
     });
+
+    if (with_boringssl.len > 0) {
+        var buf_include: [std.posix.PATH_MAX]u8 = undefined;
+        var buf_include_alloc = std.heap.FixedBufferAllocator.init(&buf_include);
+        const path_include = try std.fs.path.join(buf_include_alloc.allocator(), &.{ with_boringssl, "include" });
+
+        var buf_lib: [std.posix.PATH_MAX]u8 = undefined;
+        var buf_lib_alloc = std.heap.FixedBufferAllocator.init(&buf_lib);
+        const path_lib = try std.fs.path.join(buf_lib_alloc.allocator(), &.{ with_boringssl, "lib" });
+
+        lib_module.addIncludePath(b.path(path_include));
+        lib_module.addLibraryPath(b.path(path_lib));
+    } else {
+        lib_module.addIncludePath(.{ .cwd_relative = "/opt/homebrew/opt/openssl@3/include" });
+        lib_module.addLibraryPath(.{ .cwd_relative = "/opt/homebrew/opt/openssl@3/lib" });
+    }
+    lib_module.linkSystemLibrary("crypto", .{});
+
+    lib_module.addCSourceFiles(.{ .files = &.{"src/blind_rsa.c"} });
 
     const lib = b.addLibrary(.{
         .name = "brsa",
@@ -19,7 +39,14 @@ pub fn build(b: *std.Build) !void {
         .linkage = .static,
     });
 
-    lib.linkLibC();
+    b.installArtifact(lib);
+
+    const exe_module = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+
     if (with_boringssl.len > 0) {
         var buf_include: [std.posix.PATH_MAX]u8 = undefined;
         var buf_include_alloc = std.heap.FixedBufferAllocator.init(&buf_include);
@@ -29,49 +56,20 @@ pub fn build(b: *std.Build) !void {
         var buf_lib_alloc = std.heap.FixedBufferAllocator.init(&buf_lib);
         const path_lib = try std.fs.path.join(buf_lib_alloc.allocator(), &.{ with_boringssl, "lib" });
 
-        lib.addIncludePath(b.path(path_include));
-        lib.addLibraryPath(b.path(path_lib));
+        exe_module.addIncludePath(b.path(path_include));
+        exe_module.addLibraryPath(b.path(path_lib));
     } else {
-        lib.addIncludePath(.{ .cwd_relative = "/opt/homebrew/opt/openssl@3/include" });
-        lib.addLibraryPath(.{ .cwd_relative = "/opt/homebrew/opt/openssl@3/lib" });
+        exe_module.addIncludePath(.{ .cwd_relative = "/opt/homebrew/opt/openssl@3/include" });
+        exe_module.addLibraryPath(.{ .cwd_relative = "/opt/homebrew/opt/openssl@3/lib" });
     }
-    lib.linkSystemLibrary("crypto");
 
-    const lib_source_files = &.{"src/blind_rsa.c"};
-
-    lib.addCSourceFiles(.{ .files = lib_source_files });
-    b.installArtifact(lib);
-
-    const exe_source_files = &.{"src/test_blind_rsa.c"};
-
-    const exe_module = b.createModule(.{
-        .target = target,
-        .optimize = optimize,
-    });
+    exe_module.addCSourceFiles(.{ .files = &.{"src/test_blind_rsa.c"} });
+    exe_module.linkLibrary(lib);
 
     const exe = b.addExecutable(.{
         .name = "c-blind-rsa-signatures",
         .root_module = exe_module,
     });
-
-    if (with_boringssl.len > 0) {
-        var buf_include: [std.posix.PATH_MAX]u8 = undefined;
-        var buf_include_alloc = std.heap.FixedBufferAllocator.init(&buf_include);
-        const path_include = try std.fs.path.join(buf_include_alloc.allocator(), &.{ with_boringssl, "include" });
-
-        var buf_lib: [std.posix.PATH_MAX]u8 = undefined;
-        var buf_lib_alloc = std.heap.FixedBufferAllocator.init(&buf_lib);
-        const path_lib = try std.fs.path.join(buf_lib_alloc.allocator(), &.{ with_boringssl, "lib" });
-
-        exe.addIncludePath(b.path(path_include));
-        exe.addLibraryPath(b.path(path_lib));
-    } else {
-        exe.addIncludePath(.{ .cwd_relative = "/opt/homebrew/opt/openssl@3/include" });
-        exe.addLibraryPath(.{ .cwd_relative = "/opt/homebrew/opt/openssl@3/lib" });
-    }
-    exe.addCSourceFiles(.{ .files = exe_source_files });
-    exe.linkLibrary(lib);
-    exe.linkLibC();
 
     const run_cmd = b.addRunArtifact(exe);
 
